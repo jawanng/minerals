07e53bcc7da10397767e93cb745fe8fbee30f58d
Jawann Graves <jawann@gmail.com>
2019-05-28 22:59:48 -0400

    Added comments

--- gems/views.py
+++ gems/views.py
@@ -6,11 +6,17 @@ from .models import Gem
 
 
 def gem_list(request):
+    """Root view grabs all objects and passes to gem_list view"""
     gems = Gem.objects.all()
     return render(request, 'gems/gem_list.html', {'gems': gems})
 
 
 def gem_detail(request, name):
+    """
+    Gets an object in the form of /object and gets the record
+    or throws 404.  Blacklist is meant for the keys not to display.
+    passes the object details as a dict, blacklist, and object
+    """
     gem = get_object_or_404(Gem, name=name)
     blacklist = ('_state', 'id', 'name', 'image_filename', 'image_caption')
     return render(request, 'gems/gem_detail.html', {
@@ -18,6 +24,10 @@ def gem_detail(request, name):
 
 
 def gem_random(request):
+    """
+    Generate random integer from 1 to number of objects and passes
+    that object's name to the detail view
+    """
     count = Gem.objects.all().count()
     pk = random.randint(1, count)
     gem = Gem.objects.get(pk=pk)
